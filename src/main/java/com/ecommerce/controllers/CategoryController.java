4d60fa3b91a5fb46f29de82e23c3a4a2678a59d1
the_army_brat <sachin.kumar09@hotmail.com>
2024-11-21 20:42:57 +0530

    BUG

--- src/main/java/com/ecommerce/controllers/CategoryController.java
+++ src/main/java/com/ecommerce/controllers/CategoryController.java
@@ -8,9 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+
+import com.ecommerce.models.Category;
+import com.ecommerce.services.CategoryService;
+import com.ecommerce.services.CategoryServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +28,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/public/categories")
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(category));
+        }catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 
     @PostMapping("/admin/category")
